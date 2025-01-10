c136bf68539563b3f28c9933e6ea435bc46c39cd
Joshua Tablan <joshuatablan03@gmail.com>
2025-01-10 08:38:45 -0700

    minor updates

--- src/main.cpp
+++ src/main.cpp
@@ -2,19 +2,71 @@
 #include <LSM6DS3.h>
 #include <Wire.h>
 
-void setup() {
-  // Initialize the serial communication at 9600 baud rate
-  Serial.begin(9600); 
+BLEService ledService("19B10000-E8F2-537E-4F6C-D104768A1214"); // Bluetooth® Low Energy LED Service
 
-  // Wait for the serial monitor to open
-  while (!Serial);  // Optional, useful for some boards like Leonardo
-  
-  // Print a message to the serial monitor
-  Serial.println("Hello, PlatformIO!");
+// Bluetooth® Low Energy LED Switch Characteristic - custom 128-bit UUID, read and writable by central
+BLEByteCharacteristic switchCharacteristic("19B10001-E8F2-537E-4F6C-D104768A1214", BLERead | BLEWrite);
+
+const int ledPin = LED_BUILTIN; // pin to use for the LED
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  // set LED pin to output mode
+  pinMode(ledPin, OUTPUT);
+
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting Bluetooth® Low Energy module failed!");
+
+    while (1);
+  }
+
+  // set advertised local name and service UUID:
+  BLE.setLocalName("LED");
+  BLE.setAdvertisedService(ledService);
+
+  // add the characteristic to the service
+  ledService.addCharacteristic(switchCharacteristic);
+
+  // add service
+  BLE.addService(ledService);
+
+  // set the initial value for the characeristic:
+  switchCharacteristic.writeValue(0);
+
+  // start advertising
+  BLE.advertise();
+
+  Serial.println("BLE LED Peripheral");
 }
 
 void loop() {
-  // Keep printing a message every second
-  Serial.println("Looping...");
-  delay(1000);  // Delay for 1 second
+  // listen for Bluetooth® Low Energy peripherals to connect:
+  BLEDevice central = BLE.central();
+
+  // if a central is connected to peripheral:
+  if (central) {
+    Serial.print("Connected to central: ");
+    // print the central's MAC address:
+    Serial.println(central.address());
+
+    // while the central is still connected to peripheral:
+  while (central.connected()) {
+        if (switchCharacteristic.written()) {
+          if (switchCharacteristic.value()) {   
+            Serial.println("LED on");
+            digitalWrite(ledPin, LOW); // changed from HIGH to LOW       
+          } else {                              
+            Serial.println(F("LED off"));
+            digitalWrite(ledPin, HIGH); // changed from LOW to HIGH     
+          }
+        }
+      }
+
+    // when the central disconnects, print it out:
+    Serial.print(F("Disconnected from central: "));
+    Serial.println(central.address());
+  }
 }
