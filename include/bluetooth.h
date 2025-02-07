dc375a165ac358e32b135509c8f4b724cfeae6a3
JoshuaT35 <joshuatablan03@gmail.com>
2025-02-06 21:06:50 -0700

    added time measurement

--- include/bluetooth.h
+++ include/bluetooth.h
@@ -13,13 +13,14 @@
 
 // 6 for IMU data
 // note: number of switch characteristics must match number created
-#define SWITCH_CHARACTERISTIC_NUM 6
 #define SWITCH_CHARACTERISTIC_ACCEL_X_UUID "19B10010-E8F2-537E-4F6C-D104768A1214"
 #define SWITCH_CHARACTERISTIC_ACCEL_Y_UUID "19B10020-E8F2-537E-4F6C-D104768A1214"
 #define SWITCH_CHARACTERISTIC_ACCEL_Z_UUID "19B10030-E8F2-537E-4F6C-D104768A1214"
 #define SWITCH_CHARACTERISTIC_GYRO_X_UUID "19B10040-E8F2-537E-4F6C-D104768A1214"
 #define SWITCH_CHARACTERISTIC_GYRO_Y_UUID "19B10050-E8F2-537E-4F6C-D104768A1214"
 #define SWITCH_CHARACTERISTIC_GYRO_Z_UUID "19B10060-E8F2-537E-4F6C-D104768A1214"
+// 1 for time between readings
+#define SWITCH_CHARACTERISTIC_TIME_UUID "19B10070-E8F2-537E-4F6C-D104768A1214"
 
 // bluetooth ledService ()
 extern BLEService led_service;
@@ -31,5 +32,6 @@ extern BLEByteCharacteristic switch_characteristic_accel_z;
 extern BLEByteCharacteristic switch_characteristic_gyro_x;
 extern BLEByteCharacteristic switch_characteristic_gyro_y;
 extern BLEByteCharacteristic switch_characteristic_gyro_z;
+extern BLEByteCharacteristic switch_characteristic_time;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,8 @@
 #include "bluetooth.h"
 #include "imu-sensor.h"
 
+unsigned long previousTime = 0; // Store the last timestamp
+
 void setup() {
     setAccelerationThreshold(2.5);
 
@@ -29,6 +31,7 @@ void setup() {
     led_service.addCharacteristic(switch_characteristic_gyro_x);
     led_service.addCharacteristic(switch_characteristic_gyro_y);
     led_service.addCharacteristic(switch_characteristic_gyro_z);
+    led_service.addCharacteristic(switch_characteristic_time);
 
     // add service
     BLE.addService(led_service);
@@ -40,6 +43,7 @@ void setup() {
     switch_characteristic_gyro_x.writeValue(0);
     switch_characteristic_gyro_y.writeValue(0);
     switch_characteristic_gyro_z.writeValue(0);
+    switch_characteristic_time.writeValue(0);
 
     // start advertising
     BLE.advertise();
@@ -56,45 +60,52 @@ void loop() {
         Serial.println(central.address());
 
         // while the central is still connected to peripheral:
-    while (central.connected()) {
-        // read accelerometer data
-        aX = myIMU.readFloatAccelX();
-        aY = myIMU.readFloatAccelY();
-        aZ = myIMU.readFloatAccelZ();
+        while (central.connected()) {
+            
+            // read accelerometer data
+            aX = myIMU.readFloatAccelX();
+            aY = myIMU.readFloatAccelY();
+            aZ = myIMU.readFloatAccelZ();
 
-        // if motion is not significant, return
-        // NOTE: must be called only after reading accelerometer data
-        if (!significantMotionDetected()) {
-            return;
+            // if motion is not significant, return
+            // NOTE: must be called only after reading accelerometer data
+            if (!significantMotionDetected()) {
+                return;
+            }
+
+            unsigned long currentTime = millis(); // Get current time in milliseconds
+            unsigned long deltaTime = currentTime - previousTime; // Time difference
+
+            // read gyroscope data
+            gX = myIMU.readFloatGyroX();
+            gY = myIMU.readFloatGyroY();
+            gZ = myIMU.readFloatGyroZ();
+
+            // write the data to the corresponding switch characteristic
+            switch_characteristic_accel_x.writeValue(aX);
+            switch_characteristic_accel_y.writeValue(aY);
+            switch_characteristic_accel_z.writeValue(aZ);
+            switch_characteristic_gyro_x.writeValue(gX);
+            switch_characteristic_gyro_y.writeValue(gY);
+            switch_characteristic_gyro_z.writeValue(gZ);
+            switch_characteristic_time.writeValue(deltaTime);   // in (ms)
+
+            // debug: print the data
+            // Serial.print(aX, 3);
+            // Serial.print(',');
+            // Serial.print(aY, 3);
+            // Serial.print(',');
+            // Serial.print(aZ, 3);
+            // Serial.print(',');
+            // Serial.print(gX, 3);
+            // Serial.print(',');
+            // Serial.print(gY, 3);
+            // Serial.print(',');
+            // Serial.print(gZ, 3);
+            // Serial.print(',');
+            // Serial.print(deltaTime, 3);
+            // Serial.println();
         }
-
-        // read gyroscope data
-        gX = myIMU.readFloatGyroX();
-        gY = myIMU.readFloatGyroY();
-        gZ = myIMU.readFloatGyroZ();
-
-        // write the data to the corresponding switch characteristic
-        switch_characteristic_accel_x.writeValue(aX);
-        switch_characteristic_accel_y.writeValue(aY);
-        switch_characteristic_accel_z.writeValue(aZ);
-        switch_characteristic_gyro_x.writeValue(gX);
-        switch_characteristic_gyro_y.writeValue(gY);
-        switch_characteristic_gyro_z.writeValue(gZ);
-
-        // debug: print the data
-        Serial.print(aX, 3);
-        Serial.print(',');
-        Serial.print(aY, 3);
-        Serial.print(',');
-        Serial.print(aZ, 3);
-        Serial.print(',');
-        Serial.print(gX, 3);
-        Serial.print(',');
-        Serial.print(gY, 3);
-        Serial.print(',');
-        Serial.print(gZ, 3);
-        Serial.println();
-    }
 
         // when the central disconnects, print it out:
         Serial.print(F("Disconnected from central: "));
